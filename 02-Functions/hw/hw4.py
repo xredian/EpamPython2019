bbbceca8f02a6c3cd00a47307f614d04e337a75d
Ulyana Dyakova <ulyanadyakova@gmail.com>
2019-11-20 20:22:08 +0300

    hw4.py edited

--- 02-Functions/hw/hw4.py
+++ 02-Functions/hw/hw4.py
@@ -4,18 +4,23 @@ import inspect
 def modified_func(func, *fixated_args, **fixated_kwargs):
 
     def new_func(*fixed_args, **fixed_kwargs):
+        """
+        A func implementation of func_{func.__name__}
+        with pre-applied arguments being:
+        fixated_args values: {fixated_args}
+        fixated_kwargs values: {fixated_kwargs}
+        source_code: {code}
+        """
         frame = inspect.currentframe()
         args = inspect.getargvalues(frame)[3]
-        code = inspect.getsource(func)
-        if args['fixated_args'] is None and args['fixated_kwargs'] is None:
-            return func(*fixated_args, **fixated_kwargs)
+        code = inspect.getsource(new_func)
+        replace = [['{fixated_args}', str(args['fixated_args'])],
+                   ['{fixated_kwargs}', str(args['fixated_kwargs'])],
+                   ['{code}', code[code.rfind('\"\"\"') + 3:]],
+                   ['{func.__name__}', str(func.__name__)]]
+        for replace in replace:
+            new_func.__doc__ = new_func.__doc__.replace(*replace)
         new_func.__name__ = f'func_{func.__name__}'
-        doc = f"A func implementation of func_{new_func.__name__} " \
-              f"with pre-applied arguments being: " \
-              f"{args['fixated_args']}, {args['fixated_kwargs']}; " \
-              f"source code:" \
-              f"{code}"
-        new_func.__doc__ = doc
         new_args = [*fixed_args, *fixated_args]
         new_kwargs = {**fixed_kwargs, **fixated_kwargs}
         return func(*new_args, **new_kwargs)
@@ -29,5 +34,7 @@ def test_function(*args, **kwargs):
 
 f1 = modified_func(test_function)
 f2 = modified_func(test_function, 1, 0, **{'1': True, '0': False})
-print(f1(), f1.__doc__, f1.__name__)
-print(f2(), f2.__doc__, f2.__name__)
+f1()
+f2()
+print(f1.__doc__, f1.__name__)
+print(f2.__doc__, f2.__name__)
