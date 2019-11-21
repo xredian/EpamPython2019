222f3bdeaed50b4db1f058d292809d6af66619b3
Ulyana Dyakova <ulyanadyakova@gmail.com>
2019-11-21 23:39:52 +0300

    hw4.py edited

--- 02-Functions/hw/hw4.py
+++ 02-Functions/hw/hw4.py
@@ -11,19 +11,20 @@ def modified_func(func, *fixated_args, **fixated_kwargs):
         fixated_kwargs values: {fixated_kwargs}
         source_code: {code}
         """
-        frame = inspect.currentframe()
-        args = inspect.getargvalues(frame)[3]
-        code = inspect.getsource(new_func)
-        replace = [['{fixated_args}', str(args['fixated_args'])],
-                   ['{fixated_kwargs}', str(args['fixated_kwargs'])],
-                   ['{code}', code[code.rfind('\"\"\"') + 3:]],
-                   ['{func.__name__}', str(func.__name__)]]
-        for replace in replace:
-            new_func.__doc__ = new_func.__doc__.replace(*replace)
-        new_func.__name__ = f'func_{func.__name__}'
         new_args = [*fixed_args, *fixated_args]
         new_kwargs = {**fixed_kwargs, **fixated_kwargs}
         return func(*new_args, **new_kwargs)
+
+    frame = inspect.currentframe()
+    args = inspect.getargvalues(frame)[3]
+    code = inspect.getsource(func)
+    replace = [['{fixated_args}', str(args['fixated_args'])],
+                ['{fixated_kwargs}', str(args['fixated_kwargs'])],
+                ['{code}', code[code.rfind('\"\"\"') + 3:]],
+                ['{func.__name__}', str(func.__name__)]]
+    for replace in replace:
+        new_func.__doc__ = new_func.__doc__.replace(*replace)
+    new_func.__name__ = f'func_{func.__name__}'
 
     return new_func
 
@@ -38,3 +39,13 @@ f1()
 f2()
 print(f1.__doc__, f1.__name__)
 print(f2.__doc__, f2.__name__)
+
+
+def foo():
+    print('foo')
+
+
+mfoo = modified_func(foo)
+print(mfoo.__doc__, mfoo.__name__)
+mfoo = modified_func(foo)
+assert mfoo.__doc__ == ''
