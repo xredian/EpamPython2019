79dba8fcd84ddc827bdce3486def24a3d4e1c077
Ulyana Dyakova <ulyanadyakova@gmail.com>
2019-12-22 18:19:17 +0300

    hm1 graph done

--- 06-advanced-python/hw/task1.py
+++ 06-advanced-python/hw/task1.py
@@ -10,9 +10,29 @@ class Graph:
     def __init__(self, E):
         self.E = E
 
+    def __iter__(self):
+        self.visited = {}
+        self.queue = [list(self.E.keys())[0]]
+        return self
+
+    def __next__(self):
+        while self.queue:
+            vis = self.queue.pop(0)
+            self.visited[vis] = True
+            for elem in self.E[vis]:
+                if elem not in self.queue:
+                    try:
+                        if not self.visited[elem]:
+                            self.queue.append(elem)
+                    except KeyError:
+                        self.queue.append(elem)
+            return vis
+        raise StopIteration
+
 
 E = {'A': ['B', 'C', 'D'], 'B': ['C'], 'C': [], 'D': ['A']}
 graph = Graph(E)
 
 for vertex in graph:
     print(vertex)
+
