40d28ab545472bfb28008976b78a63ae4fb0b4b2
Ulyana Dyakova <ulyanadyakova@gmail.com>
2019-12-22 18:21:22 +0300

    hw4 printablefolder, printablefile edited

--- 06-advanced-python/hw/task4.py
+++ 06-advanced-python/hw/task4.py
@@ -18,8 +18,30 @@ class PrintableFolder:
         self.name = name
         self.content = content
 
-    def __str__(self):
-        pass
+    def __contains__(self, item):
+        values = []
+        result = False
+        for elem in self.content:
+            if isinstance(elem, PrintableFolder):
+                result = item in elem
+                values.append(elem)
+            else:
+                values.append(elem)
+
+        if item in values:
+            result = True
+
+        return result
+
+    def __str__(self, tabs=0):
+        tab = '/   ' * tabs
+        string = ''
+        for elem in self.content:
+            if isinstance(elem, PrintableFolder):
+                string = f'{string}\n{tab}|->{elem.__str__(tabs+1)}'
+            else:
+                string = f'{string}\n{tab}|->{elem}'
+        return f'V {self.name}{string}'
 
 
 class PrintableFile:
@@ -27,5 +49,18 @@ class PrintableFile:
         self.name = name
 
     def __str__(self):
-        pass
+        return str(self.name)
 
+
+if __name__ == '__main__':
+    file1 = PrintableFile("file1")
+    file2 = PrintableFile("file2")
+    file3 = PrintableFile("file3")
+
+    folder3 = PrintableFolder("folder3", [file3])
+    folder2 = PrintableFolder("folder2", [folder3, file2])
+    folder1 = PrintableFolder("folder1", [folder2, file1])
+
+    print(folder1)
+    print(file3 in folder2)
+    print(file2 in folder3)
