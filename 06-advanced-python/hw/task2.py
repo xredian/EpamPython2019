d84c9314c21f997d92e95fca9f1a5fc31fd2394c
Ulyana Dyakova <ulyanadyakova@gmail.com>
2019-12-22 18:19:50 +0300

    hw2 quaterion done

--- 06-advanced-python/hw/task2.py
+++ 06-advanced-python/hw/task2.py
@@ -12,5 +12,69 @@ https://ru.wikipedia.org/wiki/%D0%9A%D0%B2%D0%B0%D1%82%D0%B5%D1%80%D0%BD%D0%B8%D
 - взаимодействие с числами других типов
 """
 
+
 class Quaternion:
-    pass
+
+    def __init__(self, a, b, c, d):
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+
+    def __add__(self, other):
+        if isinstance(other, Quaternion):
+            a = self.a + other.a
+            b = self.b + other.b
+            c = self.c + other.c
+            d = self.d + other.d
+            return Quaternion(a, b, c, d)
+        else:
+            raise TypeError
+
+    def __mul__(self, other):
+        a1, a2 = self.a, other.a
+        b1, b2 = self.b, other.b
+        c1, c2 = self.c, other.c
+        d1, d2 = self.d, other.d
+
+        if isinstance(other, Quaternion):
+
+            a = a1 * a2 - b1 * b2 - c1 * c2 - d1 * d2
+            b = a1 * b2 + b1 * a2 + c1 * d2 - d1 * c2
+            c = a1 * c2 + c1 * a2 + d1 * b2 - b1 * d2
+            d = a1 * d2 + d1 * a2 + b1 * c2 - c1 * b2
+
+            return Quaternion(a, b, c, d)
+
+        elif isinstance(other, (float, int)):
+            return Quaternion(a1 * other, b1 * other, c1 * other, d1 * other)
+
+        else:
+            raise TypeError
+
+    def __abs__(self):
+        return (self.a**2 + self.b**2 + self.c**2 + self.d**2)**0.5
+
+    def __truediv__(self, other):
+        if isinstance(other, Quaternion):
+            adjoint = Quaternion(other.a, - other.b, - other.c, - other.d)
+            norm = Quaternion.__abs__(other)**2
+            other = adjoint / norm
+            return Quaternion.__mul__(self, other)
+        elif isinstance(other, (float, int)):
+            if other == 0:
+                raise ZeroDivisionError
+            return Quaternion(self.a / other, self.b / other, self.c / other,
+                              self.d / other)
+        else:
+            raise TypeError
+
+    def __eq__(self, other):
+        return (self.a, self.b, self.c, self.d) == (other.a, other.b, other.c,
+                                                    other.d)
+
+    def __str__(self):
+        return f'{self.a} + {self.b}i + {self.c}j + {self.d}k'
+
+    def __repr__(self):
+        return f'({self.a}, {self.b}, {self.c}, {self.d})'
