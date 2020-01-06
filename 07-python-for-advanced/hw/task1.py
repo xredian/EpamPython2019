bb18c4222eb885c228930bf91340a15d6d45d476
Ulyana Dyakova <ulyanadyakova@gmail.com>
2020-01-06 22:20:34 +0300

    hw_meta done

--- 07-python-for-advanced/hw/task1.py
+++ 07-python-for-advanced/hw/task1.py
@@ -25,3 +25,63 @@ True
 1
 
 """
+import weakref
+
+
+class Meta(type):
+
+    def __init__(cls, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        cls._insts = {}
+        cls.pool = cls._insts
+
+    def connect(cls, *args, **kwargs):
+        con_args = args + tuple(kwargs.values())
+        for key, value in cls._insts.items():
+            if value == con_args:
+                return key()
+        else:
+            print(f'There is no instance with attributes {con_args}')
+
+    def __call__(cls, *args, **kwargs):
+
+        setattr(cls, 'connect', cls.connect)
+
+        def delete(instance):
+            try:
+                del cls._insts[weakref.ref(instance)]
+            except KeyError:
+                pass
+
+        cls.__del__ = delete
+        inst = super().__call__(*args, **kwargs)
+        con_args = args + tuple(kwargs.values())
+        for key, value in cls._insts.items():
+            if value == con_args:
+                return key()
+        else:
+            cls._insts[weakref.ref(inst)] = con_args
+            return inst
+
+
+class SiamObj(metaclass=Meta):
+    def __init__(self, *args, a: int, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+        self.a = a
+
+
+if __name__ == '__main__':
+    unit1 = SiamObj('1', '2', a=1)
+    unit2 = SiamObj('1', '2', a=1)
+    print(unit1 is unit2)
+
+    unit3 = SiamObj('2', '2', a=1)
+    unit3.connect('1', '2', 1).a = 2
+    print(unit2.a == 2)
+
+    pool = unit3.pool
+    print(len(pool))
+
+    del unit3
+    print(len(pool))
